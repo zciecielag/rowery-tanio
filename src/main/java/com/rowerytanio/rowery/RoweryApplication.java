1a0603da24033ede6bc8d89e0c8690f6671bd2cc
zciecielag <zuzannaciecielag1@gmail.com>
2025-06-26 17:40:45 +0200

    Initial setup of Spring and dependencies

---
+++ src/main/java/com/rowerytanio/rowery/RoweryApplication.java
@@ -0,0 +1,13 @@
+package com.rowerytanio.rowery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rowerytanio/rowery/RoweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowerytanio.rowery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
